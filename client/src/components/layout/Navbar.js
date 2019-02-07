72d7797d1c397cc0c247d2e7c034b303e80ebd29
unknown <tfung5@gmail.com>
2019-02-07 01:00:01 -0500

    Fixed navbar showing up as pink.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
         return (
             <div className="navbar-fixed">
                 <nav className="z-depth-0">
-                    <div className="nav-wrapper-white">
+                    <div className="nav-wrapper white">
                         <Link to="/" style={ {fontFamily: "monospace"} } className="col s5 brand-logo center black-text">
                             <i className="material-icons">code</i>
                             MERN
